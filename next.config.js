6c29c9131e0b92e9ac076fb7e9493813eb81387b
SahilSoftdotin <a.sahilsoft@gmail.com>
2026-05-20 07:48:21 +0530

    update deploy workflow

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',  // Enable static export
+  basePath: '/voidlabs',  // GitHub Pages subdirectory
+  assetPrefix: '/voidlabs',  // Prefix for assets
 };
 
 export default nextConfig;
